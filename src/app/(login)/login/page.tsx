20f28ccba233d344fabe81917acf7bb047bc277e
Icaro Carneiro <contato-Devicaro@hotmail.com>
2025-03-07 17:04:06 -0300

    Criaçaõ de componentes p/ página de login

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,8 +1,19 @@
 import { InputEmailForm } from "@/app/components/Input/InputEmailForm";
+import { InputPasswordForm } from "@/app/components/Input/InputPasswordForm";
 import { Form, Formik } from "formik";
-
+import { useState } from "react";
+import * as Yup from "yup";
 
 export default function Login() {
+
+  const [formValues, setFormValues] = useState({
+    email: '',
+    senha: ''
+  })
+
+  const onSubmit = (values) => {
+    console.log(values)
+  }
   return (
     <div className="bg-[#210210] h-[730px] grid grid-cols-3">
       <div className="bg-[#ee2853] max-w-[500px] h-[500px] mt-16 col-start-2 rounded-xl">
@@ -11,13 +22,20 @@ export default function Login() {
           <p className="flex justify-center items-center p-3">Entre na sua conta ou faça seu registro</p>
         </div>
         <div className="">
-          <Formik onSubmit={ } initialValues={ }>
+          <Formik onSubmit={onSubmit}
+            initialValues={formValues}
+          >
             <Form>
               <InputEmailForm
-                name=""
+                name="email"
                 label="E-mail"
               >
               </InputEmailForm>
+              <InputPasswordForm
+                name=""
+                label="Senha"
+              >
+              </InputPasswordForm>
             </Form>
           </Formik>
         </div>
